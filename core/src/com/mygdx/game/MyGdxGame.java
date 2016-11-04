34a3f798b2a125f5350911d4d53ad0de92893659
Antonio <amgarin@my.uri.edu>
2016-11-04 22:40:28 +0100

    Score Added
    -Added ammo & score

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,11 +2,15 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
@@ -15,13 +19,33 @@ public class MyGdxGame implements ApplicationListener {
 	private Skin skin;
 	private Stage stage;
 	private Float sqLen;
+	private BitmapFont font, font2;
+	private int gold, ammo;
+	private String goldLabel, ammoLabel;
+	private GlyphLayout goldLayout, ammoLayout;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
 		skin = new Skin(Gdx.files.internal("data/uiskin.json"));
 		stage = new Stage();
-		sqLen = 200f;
+		sqLen = 200f; // CALCULATE A REASONABLE VALUE
+
+		font = new BitmapFont();
+		font.getData().setScale(4);
+		font.setColor(Color.GOLD);
+		font2 = new BitmapFont();
+		font2.getData().setScale(4);
+		font2.setColor(Color.RED);
+
+		gold = 0;
+		goldLabel = "GOLD: " + gold;
+		ammo = 10;
+		ammoLabel = "AMMO: "+ ammo;
+		goldLayout = new GlyphLayout();
+		ammoLayout = new GlyphLayout();
+		//w = Gdx.graphics.getWidth();
+		//h = Gdx.graphics.getHeight();
 
 		final TextButton button = new TextButton("Click me", skin, "default");
 		final TextButton button2 = new TextButton("Click me", skin, "default");
@@ -86,16 +110,34 @@ public class MyGdxGame implements ApplicationListener {
 	@Override
 	public void dispose() {
 		batch.dispose();
+		font.dispose();
 	}
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		//Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.begin();
+
 		stage.draw();
+
 		batch.end();
+
+		batch.begin();
+
+		goldLayout.setText(font, goldLabel);
+		float w = goldLayout.width;
+		float y = Gdx.graphics.getHeight() - goldLayout.height;
+		font.draw(batch, goldLabel, Gdx.graphics.getWidth() - w, y);
+
+		ammoLayout.setText(font, ammoLabel);
+		w = ammoLayout.width;
+		y = y - ammoLayout.height - 30f;
+		font2.draw(batch, ammoLabel, Gdx.graphics.getWidth() - w, y);
+
+		batch.end();
+
 	}
 
 	@Override
